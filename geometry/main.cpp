3ba24c132656b8685436e0f70bfd4340591b1b7d
Simona Dimitrova <nimeria1308@gmail.com>
2020-04-25 21:01:41 +0300

    added sample stuff to main

--- geometry/main.cpp
+++ geometry/main.cpp
@@ -3,12 +3,17 @@
 #include "Point.h"
 #include "Vector.h"
 #include "Line.h"
+#include "Segment.h"
+#include "Triangle.h"
 
 using namespace geometry;
 using namespace std;
 
 int main()
 {
+    // TODO
+    // MAIN APP
+
     // test Point
     Point p(1, 2, 3.1415);
 
@@ -23,4 +28,10 @@ int main()
 
     Line l(p, v2);
     cout << "line: " << l << endl;
+
+    Segment s(Point(10, 20, 30), Point(30, 40, 50));
+    cout << "segment: " << s << endl;
+
+    Triangle tr(Point(1, 2, 3), Point(10, 20, 30), Point(0, 30, 50));
+    cout << "triangle: " << tr << endl;
 }
