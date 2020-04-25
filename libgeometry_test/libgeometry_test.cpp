3b75ac40431a45d08140237ed87d21d224691fc5
Simona Dimitrova <nimeria1308@gmail.com>
2020-04-25 21:04:23 +0300

    Initial unit test drop

---
+++ libgeometry_test/libgeometry_test.cpp
@@ -0,0 +1,16 @@
+#include "pch.h"
+#include "CppUnitTest.h"
+
+using namespace Microsoft::VisualStudio::CppUnitTestFramework;
+
+namespace libgeometrytest
+{
+	TEST_CLASS(libgeometrytest)
+	{
+	public:
+		
+		TEST_METHOD(TestMethod1)
+		{
+		}
+	};
+}
