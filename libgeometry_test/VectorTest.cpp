d2d2641f1139229392716a8a7abc479b0bcd5e52
Simona Dimitrova <nimeria1308@gmail.com>
2020-04-29 01:45:49 +0300

    Finished VectorTest

--- libgeometry_test/VectorTest.cpp
+++ libgeometry_test/VectorTest.cpp
@@ -1,6 +1,9 @@
 #include "CppUnitTest.h"
 #include "Vector.h"
 
+#include <functional>
+#include <sstream>
+
 using namespace Microsoft::VisualStudio::CppUnitTestFramework;
 using namespace std;
 
@@ -45,6 +48,165 @@ namespace geometry
                 Assert::AreEqual(90.0, v.getX());
                 Assert::AreEqual(180.0, v.getY());
                 Assert::AreEqual(270.0, v.getZ());
+            }
+
+            TEST_METHOD(TestEquals)
+            {
+                Vector a(10.0, 20.0, 30.0);
+                Vector b(10.0, 20.0, 30.0);
+                Vector c(10.0, 20.0, 30.1);
+
+                Assert::IsTrue(a == Point(a));
+                Assert::IsTrue(a == b);
+                Assert::IsFalse(a == c);
+                Assert::IsFalse(Point(a) == c);
+            }
+
+            TEST_METHOD(TestLength)
+            {
+                Vector zero;
+                Vector identity(1, 0, 0);
+                Vector v1(10, 0, 0);
+                Vector v2(3, 4, 0);
+
+                Assert::AreEqual(0.0, zero.length());
+                Assert::AreEqual(1.0, identity.length());
+                Assert::AreEqual(10.0, v1.length());
+                Assert::AreEqual(5.0, v2.length());
+            }
+
+            TEST_METHOD(TestDirection)
+            {
+                Vector identity(1, 0, 0);
+                Vector v1(10, 0, 0);
+                Vector v2(3, 4, 0);
+
+                auto f = [] { Vector().direction(); };
+                Assert::ExpectException<VectorLengthException>(f);
+
+                Assert::IsTrue(identity == identity.direction());
+                Assert::IsTrue(identity.direction() == v1.direction());
+                Assert::IsTrue(Vector(0.6, 0.8, 0) == v2.direction());
+            }
+
+            TEST_METHOD(TestIsZero)
+            {
+                Vector zero;
+                Vector identity(1, 0, 0);
+
+                Assert::IsTrue(zero.isZero());
+                Assert::IsFalse(identity.isZero());
+            }
+
+            TEST_METHOD(TestIsParallel)
+            {
+                Vector v1(1, 0, 0);
+                Vector v2(4, 0, 0);
+                Vector v3(1, 1, 0);
+
+                auto f1 = [v1] { Vector().isParallel(v1); };
+                Assert::ExpectException<VectorLengthException>(f1);
+
+                auto f2 = [v1] { v1.isParallel(Vector()); };
+                Assert::ExpectException<VectorLengthException>(f2);
+
+                Assert::IsTrue(v1.isParallel(v2));
+                Assert::IsTrue(v2.isParallel(v1));
+                Assert::IsFalse(v1.isParallel(v3));
+                Assert::IsFalse(v3.isParallel(v1));
+            }
+
+            TEST_METHOD(TestIsPerpendicular)
+            {
+                Vector v1(1, 0, 0);
+                Vector v2(0, 1, 0);
+                Vector v3(4, 0, 0);
+
+                auto f1 = [v1] { Vector().isPerpendicular(v1); };
+                Assert::ExpectException<VectorLengthException>(f1);
+
+                auto f2 = [v1] { v1.isPerpendicular(Vector()); };
+                Assert::ExpectException<VectorLengthException>(f2);
+
+                Assert::IsTrue(v1.isPerpendicular(v2));
+                Assert::IsTrue(v2.isPerpendicular(v1));
+                Assert::IsFalse(v1.isPerpendicular(v3));
+                Assert::IsFalse(v3.isPerpendicular(v1));
+            }
+
+            TEST_METHOD(TestAdditionVector)
+            {
+                Vector v1(10, 20, 30);
+                Vector v2(100, 200, 300);
+                Vector v3(110, 220, 330);
+
+                Assert::IsTrue(v3 == (v1 + v2));
+                Assert::IsTrue(v3 == (v2 + v1));
+            }
+
+            TEST_METHOD(TestSubtractionVector)
+            {
+                Vector v1(110, 220, 330);
+                Vector v2(100, 200, 300);
+                Vector v3(10, 20, 30);
+                Vector v4(-10, -20, -30);
+
+                Assert::IsTrue(v3 == (v1 - v2));
+                Assert::IsTrue(v4 == (v2 - v1));
+            }
+
+            TEST_METHOD(TestMultiplicationDouble)
+            {
+                Vector v1(10, 20, 30);
+                Vector v2(40, 80, 120);
+
+                Assert::IsTrue(v2 == (v1 * 4));
+                Assert::IsTrue(v2 == (4 * v1));
+            }
+
+            TEST_METHOD(TestScalarProduct)
+            {
+                Vector v1(1, 2, 3);
+                Vector v2(3, 4, 5);
+
+                // direction does not matter
+                Assert::AreEqual(26.0, v1 * v2);
+                Assert::AreEqual(26.0, v2 * v1);
+            }
+
+            TEST_METHOD(TestVectorProduct)
+            {
+                Vector v1(1, 2, 3);
+                Vector v2(3, 4, 5);
+
+                // direction matters
+                Assert::IsTrue(Vector(-2, 4, -2) == (v1 ^ v2));
+            }
+
+            TEST_METHOD(TestMixedProduct)
+            {
+                Vector v1(1, 2, 3);
+                Vector v2(3, 4, 5);
+                Vector v3(5, 6, 7);
+
+                // v1 * (v2 ^ v3)
+                // (1,2,3) * (-2, 4, -2) = 0
+                Assert::AreEqual(0.0, v1(v2, v3));
+            }
+
+            TEST_METHOD(TestStreams)
+            {
+                Vector v1(10, -7.12, 3.1415);
+                Vector v2(40, -1.13, 5.05);
+                stringstream stream;
+                stream << v1 << " " << v2;
+
+                Vector v1_read;
+                Vector v2_read;
+                stream >> v1_read >> v2_read;
+
+                Assert::IsTrue(v1 == v1_read);
+                Assert::IsTrue(v2 == v2_read);
             }
         };
     }
