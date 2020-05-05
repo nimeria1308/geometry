63a59809c453f7f8d6f64d4427d9300c7b7c250c
Simona Dimitrova <nimeria1308@gmail.com>
2020-05-05 23:16:09 +0300

    Added some of the tests for Triangle

--- libgeometry_test/TriangleTest.cpp
+++ libgeometry_test/TriangleTest.cpp
@@ -1,5 +1,9 @@
 #include "CppUnitTest.h"
 #include "Triangle.h"
+#include "Exception.h"
+
+#include<functional>
+#include<sstream>
 
 using namespace Microsoft::VisualStudio::CppUnitTestFramework;
 using namespace std;
@@ -40,6 +44,60 @@ namespace geometry
                 Assert::AreEqual(100.0, t.getC().getX());
                 Assert::AreEqual(200.0, t.getC().getY());
                 Assert::AreEqual(300.0, t.getC().getZ());
+            }
+
+            TEST_METHOD(TestCtrSamePoints)
+            {
+                Point a(1, 2, 3);
+                Point b(a);
+                Point c(10, 20, 30);
+
+                auto f1 = [a, b, c] {Triangle t(a, b, c); };
+                Assert::ExpectException<EqualPointException>(f1);
+
+                auto f2 = [a, b, c] {Triangle t(c, a, b); };
+                Assert::ExpectException<EqualPointException>(f2);
+            }
+
+            TEST_METHOD(TestEquals)
+            {
+                Point a(1.0, 2.0, 3.0);
+                Point b(10.0, 20.0, 30.0);
+                Point c(100.0, 200.0, 300.0);
+                Point d(200.0, 200.0, 300.0);
+
+                Triangle t1(a, b, c);
+                Triangle t2(a, b, c);
+                Triangle t3(a, b, d);
+
+                Assert::IsTrue(t1 == t1);
+
+                Assert::IsTrue(t1 == t2);
+                Assert::IsTrue(t2 == t1);
+
+                Assert::IsFalse(t1 == t3);
+                Assert::IsFalse(t3 == t1);
+            }
+
+            TEST_METHOD(TestStreams)
+            {
+                Point a(1.0, 2.0, 3.0);
+                Point b(10.0, 20.0, 30.0);
+                Point c(100.0, 200.0, 300.0);
+                Point d(200.0, 200.0, 300.0);
+
+                Triangle t1(a, b, c);
+                Triangle t2(d, c, b);
+
+                stringstream stream;
+                stream << t1 << " " << t2;
+
+                Triangle t1_read;
+                Triangle t2_read;
+                stream >> t1_read >> t2_read;
+
+                Assert::IsTrue(t1 == t1_read);
+                Assert::IsTrue(t2 == t2_read);
             }
         };
     }
