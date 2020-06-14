d012d2c6934b76b5944b14974a9d8cc76db87212
Lachezar99 <64992648+Lachezar99@users.noreply.github.com>
2020-06-15 01:16:20 +0300

    Triangle cpp (ALMOST DONE)
    Не успях да реализирам тези getTypeBySide и енумите защото не ги разбирам синтактично!

--- libgeometry/Triangle.cpp
+++ libgeometry/Triangle.cpp
@@ -1,5 +1,7 @@
 #include "Triangle.h"
-#include "Exception.h"
+#include "Segment.h"
+
+
 
 using namespace geometry;
 using namespace std;
@@ -10,118 +12,167 @@ Triangle::Triangle()
 }
 
 Triangle::Triangle(const Point& a, const Point& b, const Point& c)
-    : a(a), b(b), c(c)
+	: a(a), b(b), c(c)
 {
-    // TODO
-    // proverqvane za savpadashti tochki i
-    // hvarlqne na izkliuchenie s obqsnenie
+	// !Проверка дали съвпадат точките на триъгълника!
+
+	if (a == c && b == a && c == b)
+	{
+		throw
+			EqualPointException("Points of triangle are equal");
+	}
+
+	else if (!(a == c && b == a && c == b))
+	{
+			EqualPointException("Points of triangle are not equal!");
+	}
+
 }
 
 // methods
 TriangleTypeSide Triangle::getTypeBySide() const
 {
-    // TODO
-    return TriangleTypeSide::TRIANGLE_SIDES_UNKNOWN;
+	// TODO
+
+	return TriangleTypeSide::TRIANGLE_SIDES_UNKNOWN;
 }
 
 TriangleTypeAngle Triangle::getTypeByAngle() const
 {
-    // TODO
-    return TriangleTypeAngle::TRIANGLE_ANGLES_UNKNOWN;
+	// TODO
+	return TriangleTypeAngle::TRIANGLE_ANGLES_UNKNOWN;
 }
 
+//Изчислява с Хероновата формула за лицето на триъгълника - връща лицето на 3-D триъгълника
 double Triangle::area() const
 {
-    // TODO
-    return 0;
+
+	auto p = 0.0;
+
+	p = (Segment(a, b).length() + Segment(b, c).length() + Segment(a, c).length()) / 2;
+
+	return sqrt(p * (p - Segment(a, b).length()) * (p - Segment(b, c).length()) * (p - Segment(a, c).length()));
+
 }
 
+//Изчислява параметъра на триъгълника с помощта на класа Segment където се задават арргументите като променливи и връщат параметъра с дефинираната фунцкия length
 double Triangle::perimeter() const
 {
-    // TODO
-    return 0;
+	Segment AB(a, b);
+	Segment AC(a, c);
+	Segment BC(b, c);
+
+	return AB.length() + AC.length() + BC.length();
 }
 
+//Изчислява триъгълника чрез зададените формули и връща чрез обект тип Point координатите на А, B и С
 Point Triangle::centroid() const
 {
-    // TODO
-    return Point();
+	auto coordinateA = (a.getX() + a.getY() + a.getZ()) / 3;
+	auto coordinateB = (b.getX() + b.getY() + b.getZ()) / 3;
+	auto coordinateC = (c.getX() + c.getY() + c.getZ()) / 3;
+
+
+	return Point(coordinateA, coordinateB, coordinateC);
+
 }
 
 // getters
 const Point& Triangle::getA() const
 {
-    return a;
+	return a;
 }
 
 const Point& Triangle::getB() const
 {
-    return b;
+	return b;
 }
 
 const Point& Triangle::getC() const
 {
-    return c;
+	return c;
 }
 // operators
 bool Triangle::operator==(const Triangle& other) const
 {
-    // TODO: Should compare all combinations
-    return a == other.a
-        && b == other.b
-        && c == other.c;
+	// TODO: Should compare all combinations
+	return a == other.a
+		&& b == other.b
+		&& c == other.c;
 }
 
 // tochkata leji vav vatreshnostta na triagalnika
 bool operator<(const Point& point, const Triangle& triangle)
 {
-    // TODO
-    return true;
+	if (!Segment(triangle.getA(), triangle.getB()).operator==(point))
+	{
+		Triangle t1(triangle.getA(), triangle.getB(), point);
+		Triangle t2(triangle.getA(), point, triangle.getC());
+		Triangle t3(point, triangle.getB(), triangle.getC());
+		
+		return triangle.area() == (t1.area() + t2.area() + t3.area());
+	}
+
+	return false;
 }
 
 bool operator>(const Triangle& triangle, const Point& point)
 {
-    return point < triangle;
+	return point < triangle;
 }
 
 // tochkata leji izvan triagalnika
 bool operator>(const Point& point, const Triangle& triangle)
 {
-    // TODO
-    return true;
+	if (Segment(triangle.getA(), triangle.getB()).operator==(point))
+	{
+		Triangle t1(triangle.getA(), triangle.getB(), point);
+		Triangle t2(triangle.getA(), point, triangle.getC());
+		Triangle t3(point, triangle.getB(), triangle.getC());
+
+		return triangle.area() < (t1.area() + t2.area() + t3.area());
+	}
+
+	return false;
 }
 
 bool operator<(const Triangle& triangle, const Point& point)
 {
-    return point > triangle;
+	return point > triangle;
 }
 
 // tochkata leji varhu stranite na triagalnika
 bool operator==(const Point& point, const Triangle& triangle)
 {
-    // TODO
-    return true;
+
+	if (Segment(triangle.getA(), triangle.getB()).operator==(point) || Segment(triangle.getA(), triangle.getC()).operator==(point) || Segment(triangle.getC(), triangle.getB()).operator==(point))
+	{
+		return true;
+	}
+
+	return false;
 }
 
 // tochkata leji varhu stranite na triagalnika
 bool operator==(const Triangle& triangle, const Point& point)
 {
-    return point == triangle;
+	return point == triangle;
 }
 
 ostream& operator<<(ostream& out, const Triangle& triangle)
 {
-    out << triangle.getA() << " " << triangle.getB() << " " << triangle.getC();
+	out << triangle.getA() << " " << triangle.getB() << " " << triangle.getC();
 
-    return out;
+	return out;
 }
 
 istream& operator>>(istream& in, Triangle& triangle)
 {
-    Point a, b, c;
-    in >> a >> b >> c;
+	Point a, b, c;
+	in >> a >> b >> c;
 
-    triangle = Triangle(a, b, c);
+	triangle = Triangle(a, b, c);
 
-    return in;
+	return in;
 }
+
